b6010f0498ba9a66a820891b9d48681c36cc05d9
Kovarnijpoloter <firiat69@gmail.com>
2015-02-27 22:14:44 +0300

    Добавли возращение параметро высоты и ширины картинки.

--- Geemu/src/dev/jichio/geemu/gfx/SpriteSheet.java
+++ Geemu/src/dev/jichio/geemu/gfx/SpriteSheet.java
@@ -6,11 +6,17 @@ public class SpriteSheet {
     private BufferedImage sheet;
 
     public SpriteSheet(BufferedImage sheet){
+
         this.sheet = sheet;
+
     }
+    public int height (){return sheet.getHeight();}
+    public int widht (){return sheet.getWidth();}
 
     public BufferedImage crop(int x, int y, int width, int height){
+
         return sheet.getSubimage(x, y, width, height);
+
     }
 
 }
